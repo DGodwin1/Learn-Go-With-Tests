08fe76a3f3016c0bbc2c947e762fc34f3767cd0d
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-24 21:51:14 +0100

    Use comma ok syntax a bit more and return 'bad' string if key not found.

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -1,9 +1,12 @@
 package Maps
 
-func Search(m map[string]string, k string) string{
-	value, ok := m[k]
-	if ok{
-		return value
+// 'Thin wrapper' on map that has string keys and values
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	value, ok := d[word]
+	if !ok {
+		return "word does not exist"
 	}
-	return ""
+	return value
 }
