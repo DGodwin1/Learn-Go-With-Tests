b2e02a15cd484886df09791717c626bdfedc1d2b
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-26 11:56:00 +0100

    More comments... Better to write a lot of comments that I can come back to later (for now at least) to explain precisely why the mocking works.

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,33 +2,59 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-func TestCountdown(t *testing.T){
+func TestCountdown(t *testing.T) {
 	// buffer means we can deal with std.out dependencies related
 	// to printing back results to a user. Which is handy.
-	buffer := &bytes.Buffer{}
-	spySleeper := SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := SpySleeper{}
 
-	Countdown(buffer, &spySleeper)
+		Countdown(buffer, &spySleeper)
 
-	got := buffer.String()
+		got := buffer.String()
 
-	want := `3
-2
-1
-Go!`
+		want := "3\n2\n1\nGo!"
 
-	if got != want{
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	// Faster than actual sleeping because you're not counting seconds.
-	// You're just iterating up a count. And because you call Sleep()
-	// four times in the process of executing countdown, then spySLeeper.Calls
-	// should equal 4 to reflect that sleeping 'sort of' took place.
-	if spySleeper.Calls != 4{
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
+		// Faster than actual sleeping because you're not counting seconds.
+		// You're just incrementing a count. And because you call Sleep()
+		// four times in the process of executing countdown, then spySLeeper.Calls
+		// should equal 4 to reflect that sleeping 'sort of' took place.
+		if spySleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		// Recall that Countdown takes an IOwriter and a Sleeper.
+		// The SpySleepPrinter type implements both Writer and Sleeper.
+		// When write is called on spySleepPrinter, we're actually not writing
+		// anything, we're running the append on the slice.
+		// Same is true for Sleep(). We don't actually do any sleeping. We just append
+		// the string to slice.
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v but got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
