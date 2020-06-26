a76dff889918681a6dca266058c2c854a36dd0dd
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-26 16:01:29 +0100

    Create nice makeServer() functions. Implement defer. So much more readable to use defer rather than do a close at the bottom of a particular function.

--- websiteracerSELECT/websiterace_test.go
+++ websiteracerSELECT/websiterace_test.go
@@ -1,10 +1,25 @@
 package websiteracerSELECT
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	// A server takes a ResponseWriter and a Request.
+	slowServer := makeDelayedServer(5 * time.Millisecond)
+	fastServer := makeDelayedServer(0)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	// You're setting up the slowURL to use the slow server
+	// and the fastURL to use the fast one. http.GET will call
+	// on the server you've put here.
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +27,12 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("Got %q, wanted %q", got, want)
 	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
