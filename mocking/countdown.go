c15cd96f44a4b472469c5ab13ec44adba00ae3bf
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-26 12:26:57 +0100

    whole load of mocking bits

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,6 +10,25 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
+type ConfigurableSleeper struct{
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep(){
+	//.sleep references a value in the struct that is itself a function
+	// when called, it gets the
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration){
+	s.durationSlept = duration
+}
+
 type Sleeper interface {
 	Sleep()
 }
@@ -18,30 +37,38 @@ type SpySleeper struct{
 	Calls int
 }
 
-type DefaultSleeper struct {
+type CountdownOperationsSpy struct{
+	Calls []string
 }
 
 func (s *SpySleeper) Sleep(){
 	s.Calls++
 }
 
-func (d *DefaultSleeper) Sleep(){
-	time.Sleep(1 * time.Second)
-}
 
-
-
-func main(){
+func main() {
 	out := os.Stdout
-	sleeper := &DefaultSleeper{}
-	Countdown(out, sleeper)
+	Sleeper := &ConfigurableSleeper{3 * time.Second, time.Sleep}
+	Countdown(out, Sleeper)
 }
 
 func Countdown(out io.Writer, s Sleeper){
 	for i := countdownStart; i > 0; i-- {
 		s.Sleep()
 		fmt.Fprintln(out, i)
+
 	}
 	s.Sleep()
 	fmt.Fprint(out, finalWord)
 }
+
+func (s *CountdownOperationsSpy) Sleep(){
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte)(n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
+}
+const write = "write"
+const sleep = "sleep"
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -21,14 +22,6 @@ func TestCountdown(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
-		}
-
-		// Faster than actual sleeping because you're not counting seconds.
-		// You're just incrementing a count. And because you call Sleep()
-		// four times in the process of executing countdown, then spySLeeper.Calls
-		// should equal 4 to reflect that sleeping 'sort of' took place.
-		if spySleeper.Calls != 4 {
-			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 		}
 	})
 
@@ -57,4 +50,17 @@ func TestCountdown(t *testing.T) {
 			t.Errorf("wanted calls %v but got %v", want, spySleepPrinter.Calls)
 		}
 	})
+}
+
+func TestConfigurableSleeper(t *testing.T){
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime{
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+
 }
