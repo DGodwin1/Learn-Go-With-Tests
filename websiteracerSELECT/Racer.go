bfb5d37c8523958505bd232f4f2cc5324fa0d473
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-26 16:03:03 +0100

    Refactor: response time calculator Better that those repeated lines are in one function that does some work for us.

--- websiteracerSELECT/Racer.go
+++ websiteracerSELECT/Racer.go
@@ -1,24 +1,23 @@
 package websiteracerSELECT
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 )
 
 func Racer(url1, url2 string) (fastURL string){
-	start1 := time.Now()
-	http.Get(url1)
-	url1Duration := time.Since(start1)
-	fmt.Println(url1Duration)
+	url1Duration := measureResponseTime(url1)
+	url2Duration := measureResponseTime(url2)
 
-	startB := time.Now()
-	http.Get(url2)
-	url2Duration := time.Since(startB)
-	fmt.Println(url2Duration)
 
 	if url1Duration < url2Duration{
 		return url1
 	}
 	return url2
+}
+
+func measureResponseTime(url string) time.Duration{
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
