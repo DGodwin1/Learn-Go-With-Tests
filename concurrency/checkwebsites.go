cd2c306d155b4335ed7c865b024e6a4ab25c113c
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-26 15:46:05 +0100

    Use channels to prevent race conditions

--- concurrency/checkwebsites.go
+++ concurrency/checkwebsites.go
@@ -1,11 +1,14 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		// To tell Go to start a new goroutine we turn a function call into a go
@@ -13,17 +16,21 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 		// Here, we're doing that with an anonymous function before we then perform
 		// the checking.
 		go func(u string) {
-		results[u] = wc(u)
+			resultChannel <- result{u, wc(u)} //adding a url and bool
 		}(url)
+	}
 		// The () means the function is executed at the SAME time
 		// that it is declared. All the variables that are available at the
 		// point when you declare the anonymous function are also available
 		// in the body of the function. (so, that's results, url and wc)
+		for i := 0; i < len(urls); i++ {
+			// Get a value out of the resultChannel and assign to result.
 
-	}
-
-	// Essentially give the goroutine some time.
-	time.Sleep(2 *time.Second)
+			result := <-resultChannel
+			// Now get info out of the struct with dot notation
+			// And place into the results map.
+			results[result.string] = result.bool
+		}
 
 	return results
 }
