7d0325a4b19eda9cfc18cc3fbe3364e4a612925d
David Godwin <dgodwin26@yahoo.co.uk>
2020-06-27 14:23:01 +0100

    Configurable server with timeouts set in prod and test.

--- websiteracerSELECT/Racer.go
+++ websiteracerSELECT/Racer.go
@@ -1,23 +1,38 @@
 package websiteracerSELECT
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(url1, url2 string) (fastURL string){
-	url1Duration := measureResponseTime(url1)
-	url2Duration := measureResponseTime(url2)
+var tenSecondTimeout = 10 * time.Second
 
-
-	if url1Duration < url2Duration{
-		return url1
-	}
-	return url2
+func Racer(url1, url2 string) (winner string, error error) {
+	return ConfigurableRacer(url1, url2, tenSecondTimeout)
 }
 
-func measureResponseTime(url string) time.Duration{
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ConfigurableRacer(url1, url2 string, timeout time.Duration) (fastURL string, err error){
+	select {
+	case <-ping(url1):
+		return url1, nil
+	case <-ping(url2):
+		return url2, nil
+		// If neither url1 not url2 come back before 10 seconds,
+		// then a timeout error will be thrown instead.
+		// Adding timeout to the duration means we get away
+		// from hard coding that value. Instead, we can set different
+		// timeouts depending on whether we're running in production or test.
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", url1, url2)
+	}}
+
+func ping(url string) chan struct{}{
+	ch := make(chan struct{})
+	go func(){
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
+
--- websiteracerSELECT/websiterace_test.go
+++ websiteracerSELECT/websiterace_test.go
@@ -8,27 +8,52 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	// A server takes a ResponseWriter and a Request.
-	slowServer := makeDelayedServer(5 * time.Millisecond)
-	fastServer := makeDelayedServer(0)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("Fast servers beat slow ones", func(t *testing.T) {
+		// A server takes a ResponseWriter and a Request.
+		slowServer := makeDelayedServer(5 * time.Millisecond)
+		fastServer := makeDelayedServer(0)
 
-	// You're setting up the slowURL to use the slow server
-	// and the fastURL to use the fast one. http.GET will call
-	// on the server you've put here.
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		// You're setting up the slowURL to use the slow server
+		// and the fastURL to use the fast one. http.GET will call
+		// on the server you've put here.
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	if got != want {
-		t.Errorf("Got %q, wanted %q", got, want)
-	}
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+
+		if err != nil{
+			t.Fatalf("did not expect an error but got one %v", err)
+		}
+
+		if got != want {
+			t.Errorf("Got %q, wanted %q", got, want)
+		}
+
+	})
+
+	t.Run("Timeouts return an error", func(t *testing.T){
+		serverA := makeDelayedServer(25*time.Millisecond)
+
+		defer serverA.Close()
+
+		// We can just use serverA.URL for both URL arguments
+		// because we're not really interested in that part, just
+		// that a timeout happens and is reported properly.
+		_, err := ConfigurableRacer(serverA.URL, serverA.URL, 20*time.Millisecond)
+
+		if err == nil{
+			t.Error("expected error because of timeout but didn't get one")
+		}
+	})
 
 }
+
+
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
