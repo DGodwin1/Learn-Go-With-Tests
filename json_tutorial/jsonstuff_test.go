b5c9cdc0d3ee9804c2c0d8318ea9b48cd7da709a
David Godwin <dgodwin26@yahoo.co.uk>
2020-07-02 21:54:14 +0100

    Use deep equal instead of dot notation for equality between two structs

--- json_tutorial/jsonstuff_test.go
+++ json_tutorial/jsonstuff_test.go
@@ -1,19 +1,16 @@
 package json_tutorial
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestJsonEncodesToStruct(t *testing.T){
-
 	got := ParseJSON()
 
 	want := &Config{"https://www.google.com", 100}
 
-	if got.URL != want.URL{
-		t.Errorf("Got %v, want %v", got.URL, want.URL)
+	if !reflect.DeepEqual(got, want){
+		t.Errorf("Got %v, want %v", got, want)
 	}
-
-	if got.Hits != want.Hits{
-		t.Errorf("Got %v, want %v", got.Hits, want.Hits)
-	}
-
 }
